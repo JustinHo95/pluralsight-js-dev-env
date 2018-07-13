279ad622efa9a98c3ee4db8d1d62c12614a83f00
Truyen Ho <truyenho.webdev@gmail.com>
2018-07-13 08:30:08 +0700

    update webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,14 +4,15 @@ export default {
   debug: true,
   devtool: 'inline-source-map',
   noInfo: false,
-  entry: [
+  entry: [ // địa chỉ truy cập để chuyển bó code mình viết đến
     path.resolve(__dirname, 'src/index')
+    //__dirname giống như base trong php dùng để tạo 1 đường link đầy đủ tới route yêu cầu
   ],
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'src'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js' // file các bó code sẽ compile sang để nhúng vào html
   },
   plugins: [],
   module: {
