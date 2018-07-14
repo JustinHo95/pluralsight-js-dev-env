a459e15b9837ee6b4b81748f407a4dd3e97d9ba6
Truyen Ho <truyenho.webdev@gmail.com>
2018-07-14 08:50:05 +0700

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const value = numeral(1000).format('$0,0.00');
-console.log('I would pay ' + value + ' for this awesome course!');
+window.console.log('I would pay ' + value + ' for this awesome course!');
